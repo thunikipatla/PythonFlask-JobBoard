d5338e7792c7dd0f89326365f3d446c4b8632304
srinivas.thunikipatla <srinivas.thunikipatla@cdk.com>
2019-04-02 07:14:09 +0530

    typo

--- jobs/app.py
+++ jobs/app.py
@@ -16,7 +16,7 @@ def open_connection():
 	return connection 
 
 def execute_sql(sql, values=(), commit=False, single=False):
-	connection = open_Connection()
+	connection = open_connection()
 	cursor = connection.excute(sql, values)
 	if commit == True:
 		results = connection.commit()
